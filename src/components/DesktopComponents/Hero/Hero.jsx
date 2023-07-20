972b35e65a1c6bf4f480192a8c657f86bd91d0ef
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-07-20 13:44:42 +0300

    add contacts

--- src/components/DesktopComponents/Hero/Hero.jsx
+++ src/components/DesktopComponents/Hero/Hero.jsx
@@ -3,25 +3,24 @@ import "../style/slider.css";
 import React, { useEffect, useState } from "react";
 import image1 from "../../../image/gonchar1.jpg";
 import image2 from "../../../image/majster-klas-goncharnogo-mystecztva-dlya-dvoh-01.jpg";
-import image3 from "../../../image/gonchar3.jpg";
+import image3 from "../../../image/image.webp";
 
 export const Hero = () => {
   const slides = [
     {
       image: image1,
-      title: "Верес навколо",
+      title: "Верес навКОЛО",
       subtitle: "Студія творчості",
+    },
+    {
+      image: image3,
+      title: "Верес навКОЛО",
+      subtitle: "Студія малюнку та живопису (онлайн)",
     },
     {
       image: image2,
       title: "Студія ліплення та гончарства",
       subtitle: "(для дітей та дорослих)",
-    },
-    {
-      image: image3,
-
-      title: "Верес навколо",
-      subtitle: "Студія малюнку та живопису",
     },
   ];
 
