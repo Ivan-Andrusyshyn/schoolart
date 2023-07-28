0de9286082c92b31661892993b33c157bc1c1191
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-07-28 21:40:06 +0300

    as

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { useMediaQuery, useTheme } from "@mui/material";
