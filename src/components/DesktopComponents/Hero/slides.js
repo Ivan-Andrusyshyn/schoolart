be73520dbba1138206cc1c0d5e4d7f8c4e0ad662
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-07-31 13:13:38 +0300

    qw

--- src/components/DesktopComponents/Hero/slides.js
+++ src/components/DesktopComponents/Hero/slides.js
@@ -1,7 +1,8 @@
 import image1 from "../../../image/gonchar1.jpg";
 import image2 from "../../../image/majster-klas-goncharnogo-mystecztva-dlya-dvoh-01.jpg";
 import image3 from "../../../image/image.webp";
-
+import image4 from "../../../image/gonchar3.jpg";
+import onlineImage from "../../../image/ban_ris.jpg";
 export const slides = [
   {
     image: image1,
@@ -14,7 +15,7 @@ export const slides = [
     subtitle: "Студія малюнку та живопису (онлайн)",
   },
   {
-    image: image2,
+    image: image4,
     title: "Студія ліплення та гончарства",
     subtitle: "(для дітей та дорослих)",
   },
